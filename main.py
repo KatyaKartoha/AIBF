d430063ef4f59347e5248dc0c7669d6d61276298
Katya <rybaleva_ea@mail.ru>
2024-05-30 10:32:14 +0900

    Renewal of help & start commands

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Привет! Я бот, который может генерировать картинки. Напиши /help для подробностей.\
 """)
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, """\
+Напиши текстовую инструкцию для генерации.\
+""")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
